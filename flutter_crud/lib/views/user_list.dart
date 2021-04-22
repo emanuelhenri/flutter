4d64c86dacdefdae42b5f0a2e8cb9cf755192bbe
emanuelhenri <emanuelhenrique20161@gmail.com>
2021-04-22 01:15:51 -0300

    pronto ate 07

--- flutter_crud/lib/views/user_list.dart
+++ flutter_crud/lib/views/user_list.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/user_tile.dart';
+//import 'package:flutter_crud/data/dummy_users.dart';
+import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    final users =Provider.of(context);
     return Scaffold(
       appBar: AppBar(
-        title:Text('Lista de Usuários')
+        title:Text('Lista de Usuários'),
+        actions:<Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed:(){},
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.count,
+        itemBuilder: (ctx,i) => UserTile(users.byIndex(i)),
       ),
     );
   }
