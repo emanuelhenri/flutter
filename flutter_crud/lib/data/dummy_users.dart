a5028016a06e3bba477e2c27fdb2386599be0fd6
emanuelhenri <emanuelhenrique20161@gmail.com>
2021-04-21 21:34:19 -0300

    aula01/03

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS ={
+  '1':const User(
+    id:'1',
+    name:'Maria',
+    email:'maria@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png',
+  ),
+  '2':const User(
+    id:'1',
+    name:'Rafaek',
+    email:'rafael@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
+  ),
+  '3':const User(
+    id:'1',
+    name:'Fernanda',
+    email:'rafael@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2016/04/26/07/20/woman-1353803_960_720.png',
+  ),
+  '4':const User(
+    id:'1',
+    name:'Guilherme',
+    email:'gui@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2016/04/26/07/20/woman-1353803_960_720.png',
+  ), 
+};
